abd046a14458949d8e06aa7c4e49ff619428b0e3
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2020-06-19 21:22:57 +0200

    -added additional redis cache TTLs

--- src/Constants.cs
+++ src/Constants.cs
@@ -7,5 +7,7 @@ namespace MedPark.Common
     public class Constants
     {
         public const int Day_In_Seconds = 86400;
+        public const int Hour_In_Seconds = 3600;
+        public const int HalfHour_In_Seconds = 1800;
     }
 }
